ee7d2cb6349302bc68001c98ff0930042c74b768
bhanustark <bhanustark@gmail.com>
2024-07-28 16:19:47 +0530

    added new apis

--- controllers/postController.ts
+++ controllers/postController.ts
@@ -1,3 +1,5 @@
+import { POST_PER_PAGE, POST_PER_PAGE_SITEMAP } from "../constant";
+import { Category } from "../models/Category";
 import { Post } from "../models/Post";
 import type { Post as PostType } from "../models/Post";
 
@@ -11,8 +13,41 @@ export default {
         await post.save()
         return post
     },
-    getPostsPaginated: async () => {
-        const posts = await Post.find().limit(10)
+    getPostsPaginated: async ({ params }: { params?: { pageNumber?: string, category?: string } }) => {
+        let query = Post.find()
+        if (params?.category) {
+            const categoryObject = await Category.findOne({ slug: params.category });
+            if (categoryObject)
+                query = Post.find({ categories: { $in: [categoryObject._id] } })
+        }
+        if (params?.pageNumber) {
+            query = query.skip(POST_PER_PAGE * Number(params.pageNumber))
+        }
+        query = query.limit(POST_PER_PAGE).sort({ 'createdAt': -1 })
+        return await query
+    },
+    getPostsForSitemap: async ({ params: { pageNumber } }: { params: { pageNumber: number } }) => {
+        const posts = await Post.find().limit(POST_PER_PAGE_SITEMAP).skip(POST_PER_PAGE_SITEMAP * Number(pageNumber))
         return posts
     },
+    getPostsForNewsSitemap: async ({ params: { pageNumber, category } }: { params: { pageNumber: number, category?: string } }) => {
+        if (category) {
+            const categoryObject = await Category.findOne({ slug: category });
+            if (categoryObject) {
+                const posts = await Post.find({ categories: { $in: [categoryObject._id] } }).sort({ 'createdAt': -1 }).limit(POST_PER_PAGE_SITEMAP).skip(POST_PER_PAGE_SITEMAP * Number(pageNumber))
+                return posts
+            } else {
+                throw new Error("Unknown category")
+            }
+        }
+        const posts = await Post.find().sort({ 'createdAt': -1 }).limit(POST_PER_PAGE_SITEMAP).skip(POST_PER_PAGE_SITEMAP * Number(pageNumber))
+        return posts
+    },
+    getPostBySlug: async ({ params: { slug } }: { params: { slug: string } }) => {
+        if (slug) {
+            const post = await Post.findOne({ slug })
+            return JSON.stringify(post)
+        }
+        throw new Error("slug is required")
+    }
 }
--- routes/postRoutes.ts
+++ routes/postRoutes.ts
@@ -7,5 +7,11 @@ const postRoutePath = "/api/post/"
 export default async function (app: Elysia) {
     app.post(`${postRoutePath}add`, postController.addPost, postRoutesSchemas.addPostRouteSchema)
     app.get(`${postRoutePath}get`, postController.getPostsPaginated, postRoutesSchemas.getPostsPaginatedRouteSchema)
+    app.get(`${postRoutePath}get/:pageNumber`, postController.getPostsPaginated, postRoutesSchemas.getPostsPaginatedRouteSchema)
+    app.get(`${postRoutePath}get/sitemap/:pageNumber`, postController.getPostsForSitemap, postRoutesSchemas.getPostsPaginatedRouteSchema)
+    app.get(`${postRoutePath}get/newssitemap/:pageNumber`, postController.getPostsForNewsSitemap, postRoutesSchemas.getPostsPaginatedRouteSchema)
+    app.get(`${postRoutePath}get/newssitemap/:pageNumber/:category`, postController.getPostsForNewsSitemap, postRoutesSchemas.getPostsPaginatedRouteSchema)
+    app.get(`${postRoutePath}get/:pageNumber/:category`, postController.getPostsPaginated, postRoutesSchemas.getPostsPaginatedRouteSchema)
+    app.get(`${postRoutePath}byslug/:slug`, postController.getPostBySlug, postRoutesSchemas.getPostsPaginatedRouteSchema)
     return app
 }
